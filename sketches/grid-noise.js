1f1ef434f8be4fff81b7a6c361359c1deed58dcf
Kyle Stewart <kylestew@gmail.com>
2023-11-28 13:18:43 -0700

    Centered text in boxes

--- sketches/grid-noise.js
+++ sketches/grid-noise.js
@@ -15,15 +15,15 @@ const params = {
     margin: 0.333,
 
     // glyph settings
-    scaleMin: 1,
-    scaleMax: 30,
-    lineCap: "butt",
+    // scaleMin: 1,
+    // scaleMax: 30,
+    // lineCap: "butt",
 
     // noise settings
-    freq: 0.001,
-    amp: 0.2,
-    animate: true,
-    frame: 0,
+    // freq: 0.001,
+    // amp: 0.2,
+    // animate: true,
+    // frame: 0,
 };
 
 /**
@@ -50,11 +50,13 @@ const makeGridPositions = (cols, rows, gridW, gridH, margX, margY) => {
     const cellH = gridH / rows;
 
     let positions = [];
+    let idx = 0;
     for (let i = 0; i < numCells; i++) {
         const col = i % cols;
         const row = Math.floor(i / cols);
 
         positions.push({
+            idx: idx++,
             col,
             row,
             x: margX + col * cellW,
@@ -66,7 +68,7 @@ const makeGridPositions = (cols, rows, gridW, gridH, margX, margY) => {
     return positions;
 };
 
-const drawThing = (ctx, width, height) => {
+const drawSquare = (ctx, width, height) => {
     ctx.beginPath();
     ctx.rect(0, 0, width, height);
     ctx.fill();
@@ -88,43 +90,70 @@ const sketch = () => {
         );
 
         context.fillStyle = "#ff0000";
-        // context.lineWidth = 10.0;
+        context.strokeStyle = "#fff";
+        context.font = "100px serif";
+        context.textBaseline = "top";
+        // context.textAlign = "center";
 
-        pos.forEach(({ col, row, x, y, w, h }) => {
+        pos.forEach(({ idx, col, row, x, y, w, h }) => {
             context.save();
             context.translate(x, y);
 
-            drawThing(context, w * 0.5, h * 0.5);
+            // grid cell box
+            context.strokeStyle = "#fff";
+            context.lineWidth = 2;
+            context.strokeRect(0, 0, w, h);
+
+            // measure text
+            const text = idx.toString();
+            const metrics = context.measureText(text);
+
+            const mx = metrics.actualBoundingBoxLeft * -1;
+            const my = metrics.actualBoundingBoxAscent * -1;
+            const mw = metrics.actualBoundingBoxLeft + metrics.actualBoundingBoxRight;
+            const mh = metrics.actualBoundingBoxAscent + metrics.actualBoundingBoxDescent;
+
+            // center text
+            const tx = (w - mw) * 0.5 - mx;
+            const ty = (h - mh) * 0.5 - my;
+            context.translate(tx, ty);
+
+            // draw text outline
+            context.strokeStyle = "#00ff00";
+            context.strokeRect(mx, my, mw, mh);
+
+            // draw text
+            context.fillText(text, 0, 0);
 
             context.restore();
         });
 
-        context.fillStyle = "#00ff00";
-        context.globalCompositeOperation = "lighter"; // adds colors together
+        // context.fillStyle = "#00ff00";
+        // context.globalCompositeOperation = "lighter"; // adds colors together
 
-        pos.forEach(({ col, row, x, y, w, h }) => {
-            context.save();
-            // context.translate(x, y);
-            context.translate(x + w * 0.05, y + h * 0.05);
+        // pos.forEach(({ col, row, x, y, w, h }) => {
+        //     context.save();
+        //     // context.translate(x, y);
+        //     context.translate(x + w * 0.05, y + h * 0.05);
 
-            drawThing(context, w * 0.5, h * 0.5);
+        //     drawThing(context, w * 0.5, h * 0.5);
 
-            context.restore();
-        });
+        //     context.restore();
+        // });
 
-        context.fillStyle = "#0000ff";
-        context.globalCompositeOperation = "lighter";
+        // context.fillStyle = "#0000ff";
+        // context.globalCompositeOperation = "lighter";
 
-        pos.forEach(({ col, row, x, y, w, h }) => {
-            context.save();
-            // context.translate(x, y);
-            // context.translate(x + w * 0.05, y + h * 0.05);
-            context.translate(x + w * 0.1, y + h * 0.1);
+        // pos.forEach(({ col, row, x, y, w, h }) => {
+        //     context.save();
+        //     // context.translate(x, y);
+        //     // context.translate(x + w * 0.05, y + h * 0.05);
+        //     context.translate(x + w * 0.1, y + h * 0.1);
 
-            drawThing(context, w * 0.5, h * 0.5);
+        //     drawThing(context, w * 0.5, h * 0.5);
 
-            context.restore();
-        });
+        //     context.restore();
+        // });
 
         // for (let i = 0; i < numCells; i++) {
 
@@ -166,18 +195,18 @@ const createPane = () => {
     folder.addInput(params, "rows", { min: 2, max: 50, step: 1 });
     folder.addInput(params, "margin", { min: 0.0, max: 1.0 });
 
-    folder = pane.addFolder({ title: "Symbol" });
-    folder.addInput(params, "lineCap", {
-        options: { butt: "butt", round: "round", square: "square" },
-    });
-    folder.addInput(params, "scaleMin", { min: 1, max: 100 });
-    folder.addInput(params, "scaleMax", { min: 1, max: 100 });
+    // folder = pane.addFolder({ title: "Symbol" });
+    // folder.addInput(params, "lineCap", {
+    //     options: { butt: "butt", round: "round", square: "square" },
+    // });
+    // folder.addInput(params, "scaleMin", { min: 1, max: 100 });
+    // folder.addInput(params, "scaleMax", { min: 1, max: 100 });
 
-    folder = pane.addFolder({ title: "Noise" });
-    folder.addInput(params, "freq", { min: -0.01, max: 0.01 });
-    folder.addInput(params, "amp", { min: 0, max: 1 });
-    folder.addInput(params, "animate");
-    folder.addInput(params, "frame", { min: 0, max: 999 });
+    // folder = pane.addFolder({ title: "Noise" });
+    // folder.addInput(params, "freq", { min: -0.01, max: 0.01 });
+    // folder.addInput(params, "amp", { min: 0, max: 1 });
+    // folder.addInput(params, "animate");
+    // folder.addInput(params, "frame", { min: 0, max: 999 });
 };
 
 createPane();
